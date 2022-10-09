25e3deebc50ba87928fc54f2af669099b543288c
dangngo1709 <112128470+dangngo1709@users.noreply.github.com>
2022-10-08 19:08:06 -0700

    Update SubArray.java

--- src/SubArray.java
+++ src/SubArray.java
@@ -2,20 +2,20 @@ public class SubArray {
 	static int maxSubArray(  int array[], int L,int M, int R) {
 		int Sum = 0;
 		int leftSum  = 0;
-
 	for (int i = M; i >= L; i--) {
 		Sum = Sum + array[i];
 		if (leftSum < Sum)
 			leftSum = Sum;
+			
 	}
 	Sum = 0;
 	int rightSum = 0 ;
 	for ( int j = M+1; j <= R; j++ ) {
 		Sum += array [j];
 		if(rightSum < Sum)
-			rightSum = Sum;	
+			rightSum = Sum;;
 	}
-	
+
 
 		return leftSum + rightSum;
 	
@@ -24,6 +24,7 @@ public class SubArray {
 	static int maxSum(int array[], int L, int R) {
 		if(L == R) {
 			 return array[L];
+		
 		}
 		int M = (L +R)/2;
 		int maxLeft = maxSum(array,L,M);
@@ -32,16 +33,19 @@ public class SubArray {
 	
 		if (maxLeft> maxRight && maxLeft > finalmaxSum)
 			return maxLeft;
-		if (maxRight> maxLeft && maxRight > finalmaxSum)
-			return maxRight;
-		else return finalmaxSum;
+		if (maxRight> maxLeft && maxRight > finalmaxSum) {
+			return maxRight;}
 		
-		}
+		else return finalmaxSum;
+	
+	
+	}
+	
+	
 		 
 	public static void main(String[] args) {
-	    int array[] = {3, -1, -1, 10, -3, -2, 4};
-	    System.out.println(maxSum(array, 0, 6));
+	    int array[] = {2-2, -3, 4, -1, -2, 1, 5, -3, 7, 2, 6};
+	    System.out.println(maxSum(array, 0, array.length -1));
 	  }
 	}	
 	
-
