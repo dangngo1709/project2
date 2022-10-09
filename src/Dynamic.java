4e1e187c9194353d59fbe7b125353ed5f7d5fe90
dangngo1709 <112128470+dangngo1709@users.noreply.github.com>
2022-10-08 19:07:11 -0700

    Update Dynamic.java

--- src/Dynamic.java
+++ src/Dynamic.java
@@ -3,25 +3,68 @@ public class Dynamic {
 	public int Dynamic(int[] array) {
 		int totalSum = 0;
 		int currSum = 0;
-
+		int depart = 0;
+		int arrive = 0;
+		int tempDepart = 0;
 		for (int i = 0; i < array.length; i++) {
 			currSum = currSum + array[i];
-			if (currSum < 0) {
-				currSum = 0;
-
-			}
 			if (currSum > totalSum) {
 				totalSum = currSum;
+				depart = tempDepart;
+				arrive = i;
+			}
+
+			if (currSum < 0) {
+				currSum = 0;
+				tempDepart = i+1;
 
 			}
-			
+
 		}
+		System.out.println(depart);
+		System.out.println(arrive);
 		return totalSum;
 	}
-	public static void main (String[] args) {
+
+	public static void main(String[] args) {
 		Dynamic max = new Dynamic();
-		int array[] = {3, -1, -1, 10, -3, -2, 4};
+		int array[] = { 2 - 2, -3, 4, -1, -2, 1, 5, -3, 7, 2,-6,10, -2 };
 		int Sum = max.Dynamic(array);
 		System.out.print(Sum);
+	}
 }
+
+public class Dynamic {
+	public int Dynamic(int[] array) {
+		int totalSum = 0;
+		int currSum = 0;
+		int depart = 0;
+		int arrive = 0;
+		int tempDepart = 0;
+		for (int i = 0; i < array.length; i++) {
+			currSum = currSum + array[i];
+			if (currSum > totalSum) {
+				totalSum = currSum;
+				depart = tempDepart;
+				arrive = i;
+			}
+
+			if (currSum < 0) {
+				currSum = 0;
+				tempDepart = i+1;
+
+			}
+
+		}
+		System.out.println(depart);
+		System.out.println(arrive);
+		return totalSum;
+	}
+
+	public static void main(String[] args) {
+		Dynamic max = new Dynamic();
+		int array[] = { 2 - 2, -3, 4, -1, -2, 1, 5, -3, 7, 2,-6,10, -2 };
+		int Sum = max.Dynamic(array);
+		System.out.print(Sum);
+	}
 }
