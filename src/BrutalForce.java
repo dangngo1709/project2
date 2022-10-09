f05411e00ad0c646a689194d49028a19d509563b
dangngo1709 <112128470+dangngo1709@users.noreply.github.com>
2022-10-08 19:07:51 -0700

    Update BrutalForce.java

--- src/BrutalForce.java
+++ src/BrutalForce.java
@@ -1,8 +1,12 @@
 
 public class BrutalForce {
+
+	
 public int BrutalForce( int[] array) {
-		int totalSum = 0;
 		
+	int totalSum = 0;
+	int depart = -1;
+	int arrive = -1;
 	
 		if (array == null || array.length == 0) {
 			return totalSum;
@@ -14,19 +18,23 @@ public int BrutalForce( int[] array) {
 				currSum = currSum + array[j];
 				if (currSum > totalSum) {
 					totalSum = currSum;
+					depart = i;
+					arrive = j;
 					
 				}
 
 			}
 
 		}
+		System.out.println(depart);
+		System.out.println(arrive);
 
 		return totalSum;
 	}
 
 public static void main (String[] args) {
 BrutalForce max = new BrutalForce();
-int array[] = {3, -1, -1, 10, -3, -2, 4};
+int array[] = {2 - 2, -3, 4, -1, -2, 1, 5, -3, 7, 2,-6,10, -2};
 int Sum = max.BrutalForce(array);
 System.out.print(Sum);
 }
